869b32cd12455121952b12e9f8590d8c84373c41
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-04-17 15:33:02 +0430

    Initial commit v 1.0

---
+++ openganjoor.js
@@ -0,0 +1,22 @@
+function createPopup(url) {
+    chrome.windows.create({
+        url: 'https://ganjoor.net?pinterest_url='+url,
+        width: 800,
+        height: 600,
+        type: 'popup'
+    });
+}
+
+browser.browserAction.onClicked.addListener((tab) => {
+    if(tab.url.includes('pinterest.com')){
+        createPopup(tab.url);
+    }
+    else{
+        chrome.windows.create({
+            url: 'html/error.html',
+            width: 800,
+            height: 600,
+            type: 'popup'
+        });
+    }
+})
